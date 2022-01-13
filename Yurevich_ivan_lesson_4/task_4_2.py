a2a92a8e251d8e7c3b0591b5e30a6a89c65f8a7c
YurevichIvan <yurevich_ivah@mail.ru>
2022-01-13 22:11:48 +0300

    Homework Lesson 4

---
+++ Yurevich_ivan_lesson_4/task_4_2.py
@@ -0,0 +1,41 @@
+from requests import get
+import decimal
+
+URL = 'http://www.cbr.ru/scripts/XML_daily.asp'
+
+def currency_rates(code: str) -> float:
+    """возвращает курс валюты `code` по отношению к рублю
+    @rtype: object
+    """
+    code = code.upper()
+
+    response = get(URL)
+    #if response.status_code == 200: # Можно раскоментировать для проверки запроса.
+        #print('Запрос успешен')
+    #else:
+        #print(f'Запрос не успешен: {response.status_code}')
+
+    content = response.content.decode(encoding=response.encoding) # приводим содержимое ответа сервера к строковому виду,
+                                                              # может есть способ проще? чтоб понять как это сделать было потрачено очень много времени)
+    for i in content.split('<CharCode>')[1:]:
+        charcode = i[:3]
+        value = ((i.split('<Value>')[1:][0]).split('</Value>')[0]).split(',')
+        result_value = round(decimal.Decimal('.'.join(value)) * 1,2)
+        if code == charcode:
+            return float(result_value)
+
+
+#print(currency_rates("AUD"))
+print(currency_rates("AzN"))
+print(currency_rates("XDR"))
+#print(currency_rates("euR"))
+#print(currency_rates("1"))
+#print(currency_rates("e"))
+#print(currency_rates("noname"))
+
+
+
+
+
+
+
---
+++ Yurevich_ivan_lesson_4/task_4_4.py
@@ -0,0 +1,12 @@
+#from utils import currency_rates
+import utils
+#print(currency_rates('eur'))
+print(utils.currency_rates('usd'))
+print(utils.currency_rates('eur'))
+print(utils.currency_rates('gbp'))
+
+#(virt) C:\Users\Sonya\PycharmProjects\Homework_python\Yurevich_ivan_lesson_4>python task_4_4.py
+#74.53
+#84.67
+#17.12
+
